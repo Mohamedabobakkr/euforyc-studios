9f0322ed24bf02ad7207a8784177ad3d13b4b064
Mohamed Abobakr <mohamedabobakr@Mohameds-MacBook-Pro.local>
2025-07-27 21:24:50 +0300

    Move Google Analytics to layout for all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -168,23 +168,19 @@ export default function RootLayout({
         </main>
         <Footer />
         
-        {/* Google Analytics - Add your GA ID */}
-        {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FGLFB14Q0J"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FGLFB14Q0J');
+          `}
+        </Script>
       </body>
     </html>
   );
